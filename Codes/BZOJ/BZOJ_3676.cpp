1c1f5fdeb11b847ab83b880d7b3fd6af0b6ff781
hzoi-wildrage <wangxiaohui0821@gmail.com>
2019-08-01 17:13:21 +0800

    Update

--- Codes/BZOJ/BZOJ_3676.cpp
+++ Codes/BZOJ/BZOJ_3676.cpp
@@ -1,40 +1,42 @@
-#include <cstring>
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 using namespace std;
-const int MAXN = 3e5 + 10;
-char ch[MAXN];
-struct Node
-{
-	int cnt, now, fail[MAXN], len[MAXN], size[MAXN], tr[MAXN][26];
-	Node() {cnt = 1; fail[0] = fail[1] = 1; len[1] = -1;}
-	void extend(int x, int pos)
-	{
-		int p = now;
-		while (ch[pos - len[p] - 1] != ch[pos]) p = fail[p];
-		if (!tr[p][x])
-		{
-			int np=++cnt, q = fail[p];
-			len[np] = len[p] + 2;
-			while (ch[pos - len[q] - 1] != ch[pos]) q = fail[q];
-			fail[np] = tr[q][x];
-			tr[p][x] = np;
-		}
-		now = tr[p][x];
+const int MAXN = 300000 + 5;
+char s[MAXN];
+struct PAM {
+  int len[MAXN << 1], fail[MAXN << 1], size[MAXN], tr[MAXN << 1][26];
+  int cnt, now;
+  PAM() {
+    cnt = 1;
+    fail[0] = fail[1] = 1;
+    len[1] = -1;
+  }
+  int extend(int c, int pos) {
+    int p = now;
+    while (s[pos - len[p] - 1] != s[pos]) p = fail[p];
+    if (!tr[p][c]) {
+      int np = ++cnt, q = fail[p];
+      len[np] = len[p] + 2;
+      while (s[pos - len[p] - 1] != s[pos]) q = fail[q];
+      fail[np] = tr[q][c];
+      tr[p][c] = np;
+    }
+    now = tr[p][c];
 		size[now]++;
-	}
-}PAM;
-
-int main()
-{
-	scanf ("%s", ch + 1);
-	int n = strlen(ch + 1);
-	for (int i = 1; i <= n; i++) PAM.extend(ch[i] - 'a', i);
-	long long ans = 0;
-	for (int i = PAM.cnt; i; i--)
-	{
-		PAM.size[PAM.fail[i]] += PAM.size[i];
-		ans = max(ans, 1ll * PAM.size[i] * PAM.len[i]);
-	}
-	printf ("%lld\n", ans);
+    return pos - len[now];
+  }
+}pam;
+int main() {
+  scanf ("%s", s + 1);
+  int n = strlen(s + 1);
+  for (int i = 1; i <= n; i++)
+    pam.extend(s[i], i);
+  long long ans = 0;
+  for (int i = 1; i <= n; i++) {
+    pam.size[pam.fail[i]] += pam.size[i];
+    ans = max(ans, 1ll * pam.size[i] * pam.len[i]);
+  }
+  printf ("%lld\n", ans);
+  while (1);
 }
