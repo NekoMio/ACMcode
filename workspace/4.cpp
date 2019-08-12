239af747c9ccd886e25c78fff7c05eecca7fa812
NekoMio <hui@nekomio.com>
2019-08-12 09:58:41 +0800

    Upd

--- workspace/4.cpp
+++ workspace/4.cpp
@@ -2,19 +2,6 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
-inline int read() {
-  int x = 0, f = 1;
-  char ch = getchar();
-  while (ch < '0' || ch > '9') {
-    if (ch == '-') f = -1;
-    ch = getchar();
-  }
-  while (ch >= '0' && ch <= '9') {
-    x = x * 10 + ch - '0';
-    ch = getchar();
-  }
-  return x * f;
-}
 char mp[2005][2005];
 int Sum[2005][2005], Sum2[2005][2005];
 int Num[2005][2005], Num2[2005][2005];
@@ -32,7 +19,7 @@ int main() {
     for (int i = 1; i <= n; i++)
       Num[i][j] = Num[i - 1][j] + (mp[i][j] == 'B');
     Num[n + 1][j] = Num[n][j] == 0;
-    Num[n + 1][j] += Sum[n + 1][j - 1];
+    Num[n + 1][j] += Num[n + 1][j - 1];
   }
   for (int j = 1; j <= n - k + 1; j++)
     for (int i = 1; i <= n; i++)
@@ -40,25 +27,7 @@ int main() {
   for (int i = 1; i <= n - k + 1; i++)
     for (int j = 1; j <= n; j++)
       Num2[i][j] = Num2[i][j - 1] + (Num[i - 1][j] == 0 && (Num[n][j] - Num[i + k - 1][j]) == 0);
-  int ans = 0;
-  // for (int i = 1; i <= n; i++) {
-  //   for (int j = 1; j <= n + 1; j++) {
-  //     printf ("%d ", Sum[i][j]);
-  //   }
-  //   printf ("\n");
-  // }
-  // for (int i = 1; i <= n + 1; i++) {
-  //   for (int j = 1; j <= n; j++) {
-  //     printf ("%d ", Num[i][j]);
-  //   }
-  //   printf ("\n");
-  // }
-  // for (int i = 1; i <= n; i++) {
-  //   for (int j = 1; j <= n; j++) {
-  //     printf ("%d ", Sum2[i][j]);
-  //   }
-  //   printf ("\n");
-  // }
+  int ans = 0
   for (int i = 1; i <= n - k + 1; i++) {
     for (int j = 1; j <= n - k + 1; j++) {
       ans = max(ans, Sum2[i + k - 1][j] - Sum2[i - 1][j] + Num2[i][j + k - 1] - Num2[i][j - 1] + \
