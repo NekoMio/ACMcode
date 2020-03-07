a31c0a361d3ab1ad22612785d7e59dad8e4098e5
hzoi-wildrage <wangxiaohui0821@gmail.com>
2020-03-07 11:27:48 +0800

    Upd

--- test/G.cpp
+++ test/G.cpp
@@ -7,22 +7,23 @@ void DFS(int base, int dep) {
   if (dep == 9) return;
   for (int i = 0; i <= 2; i++) {
     vet[++t] = base + bin[dep] * num[i];
-    DFS(base + bin[dep] * num[i], dep + 1);
+    if (i != 0)
+      DFS(base + bin[dep] * num[i], dep + 1);
   }
 }
 int Mer(int L, int R, int l, int r) {
-  if (R <= l || r <= l) return 0;
+  if (R < l || r < L) return 0;
 
   if (L <= l && R <= r) return R - l + 1;
   if (l <= L && r <= R) return r - L + 1;
   if (L <= l && r <= R) return r - l + 1;
-  if (l <= L && R <= l) return R - L + 1;
+  if (l <= L && R <= r) return R - L + 1;
   return 0;
 }
 long long Calc(int L, int R, int l, int r, int k) {
   int start = lower_bound(vet + 1, vet + t + 1, L) - vet;
   long long ret = 0;
-  for (int i = start - 1; vet[i] < R; i++) {
+  for (int i = start - 1; vet[i] < R && i + k <= t; i++) {
     ret += 1ll * Mer(vet[i] + 1, vet[i + 1], L, R) * Mer(vet[i + k], vet[i + k + 1] - 1, l, r);
   }
   return ret;
@@ -30,13 +31,21 @@ long long Calc(int L, int R, int l, int r, int k) {
 int main() {
   int pl, pr, vl, vr, k;
   scanf ("%d%d%d%d%d", &pl, &pr, &vl, &vr, &k);
-  printf("----------\n");
+  // printf("----------\n");
   DFS(4, 1);
   DFS(7, 1);
   sort(vet + 1, vet + t + 1);
+  t = unique(vet + 1, vet + t + 1) - vet - 1;
+  vet[t + 1] = 0x3f3f3f3f;
+  // for (int i = 1; i <= t; i++) printf ("%d\n", vet[i]);
   long long ans = 0;
   ans += Calc(pl, pr, vl, vr, k);
   ans += Calc(vl, vr, pl, pr, k);
-  printf ("%d\n", ans);
+  if (k == 1) {
+    for (int i = 1; i <= t; i++)
+      if (vet[i] <= pr && vet[i] >= pl && vet[i] <= vr && vet[i] >= vl)
+        ans--;
+  }
+  // printf ("%d\n", ans);
   printf ("%.9f\n", 1.0 * ans / (1. * (pr - pl + 1) * (vr - vl + 1)));
 }
